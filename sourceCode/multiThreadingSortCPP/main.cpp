8e274d725c9c03c4242e74ceb2576f280b2f8b86
zoserqh <zoserqh@gmail.com>
2024-08-15 22:08:20 -0500

    use of threads for readList

--- sourceCode/multiThreadingSortCPP/main.cpp
+++ sourceCode/multiThreadingSortCPP/main.cpp
@@ -1,5 +1,5 @@
-//g++ main.cpp -o ../../bin/mtCPP
-// reading file: 23.7453sec
+//g++ main.cpp -pthread -o ../../bin/mtCPP
+// reading file: 5.65481sec
 // filling buckets: 5.09247sec
 // sorting buckets with multi-threading: 9.71529sec
 // grouping buckets: 1.0708sec
@@ -13,6 +13,9 @@
 #include <iomanip>
 #include <chrono>
 #include<thread>
+#include <filesystem>
+#include <cstdint>
+#include <cstring>
 
 void readList(std::vector<double>& v);
 void sortList(std::vector<double>& v);
@@ -20,6 +23,7 @@ void fillBuckets(std::vector<double>& v,std::vector<std::vector<double>>& bucket
 void mtSorting(std::vector<std::vector<double>>& buckets);
 void sortingBuckts(std::vector<double>* b);
 void grouping(std::vector<double>& v,std::vector<std::vector<double>>& buckets);
+void mtRead(char* ptrb,double* ptra,int lengthLine,int arrSize, int nroThreads);
 
 int main(){
 
@@ -32,25 +36,51 @@ int main(){
     for(int i=1;i<v.size();i++){
         if(v[i-1]>v[i]){ std::cout<<"unsorted\n"; }
     }
+
+    // std::cout << std::fixed << std::setprecision(20);
+    // for(int i=0;i<v.size();i++){
+    //     std::cout << v[i] << " ";
+    // } std::cout << "\n";
+
     return 0;
 }
 
 void readList(std::vector<double>& v){
-    std::string line;
-    std::ifstream myfile ("../data/list.txt");
-
-    auto start = std::chrono::high_resolution_clock::now();  
-    // unsync the I/O of C and C++.
-    // std::ios_base::sync_with_stdio(false);
-
-    if (myfile.is_open()){
-        while ( getline (myfile,line) ){
-            v.push_back(std::stod(line));
-        }
-        myfile.close();
-    }
-    else std::cout << "Unable to open file"; 
+    auto start = std::chrono::high_resolution_clock::now();
     
+    std::string pathFile = "../data/list.txt";
+    std::ifstream myFile (pathFile);
+    unsigned long int bufferSize = std::filesystem::file_size(pathFile);
+    std::cout << "File size is: "<< bufferSize << " bytes" << std::endl;
+    std::unique_ptr<char[]> buffer(new char[bufferSize]);
+    
+    if (myFile.is_open()){
+        myFile.read(buffer.get(), bufferSize);
+
+        //each line have one number with 22 digits + 1 \n = 23
+        int lengthLine = 23;
+        int arrSize = bufferSize/lengthLine;
+        std::unique_ptr<double[]> arr(new double[arrSize]);
+        
+        //multiThreading using 10 threads
+        int nroThreads = 10;
+        char* ptrB[nroThreads]; // pointer to buffer
+        double* ptrA[nroThreads]; // pointer to array
+        std::thread myThreads[nroThreads];
+        for(int i=0;i<nroThreads;i++){
+            ptrB[i]=&buffer[i*(bufferSize/nroThreads)];
+            ptrA[i]=&arr[i*(arrSize/nroThreads)];
+            myThreads[i] = std::thread(mtRead,ptrB[i],ptrA[i],lengthLine,arrSize,nroThreads);
+        }
+
+        for(int i=0;i<nroThreads;i++){
+            myThreads[i].join();
+        }
+
+        v.insert(v.begin(),ptrA[0],ptrA[0]+arrSize);
+    }
+    else {  std::cout << "Unable to open file"; }
+
     auto end = std::chrono::high_resolution_clock::now();
     double time_taken = ( std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() ) * 1e-9;
     std::cout << "reading file: "<< time_taken << "sec\n";
@@ -152,4 +182,16 @@ void grouping(std::vector<double>& v,std::vector<std::vector<double>>& buckets){
     auto end = std::chrono::high_resolution_clock::now();
     double time_taken = ( std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() ) * 1e-9;
     std::cout << "grouping buckets: " << time_taken << "sec\n";
+}
+
+void mtRead(char* ptrb, double* ptra, int lengthLine, int arrSize, int nroThreads){
+    for(int i=0;i<arrSize/nroThreads;i++){
+        char number[lengthLine-1];
+        std::memcpy(number,ptrb,lengthLine-1);
+        *ptra=std::atof(number);
+
+        ptrb += lengthLine;
+        ptra += 1;
+    }
+    // std::cout<< "id: "<<std::this_thread::get_id()<< " "<< *(ptrb+2) <<"\n";
 }
--- sourceCode/sortDefaultCPP/main.cpp
+++ sourceCode/sortDefaultCPP/main.cpp
@@ -1,5 +1,5 @@
-// g++ main.cpp -o ../../bin/stlCPP
-// reading 27 sec | sorting 55 sec
+// g++ main.cpp -pthread -o ../../bin/stlCPP
+// reading 5.62197sec | sorting 55 sec
 
 #include<string>
 #include<algorithm>
@@ -8,10 +8,14 @@
 #include<vector>
 #include <iomanip>
 #include <chrono>
+#include<thread>
+#include <filesystem>
+#include <cstdint>
+#include <cstring>
 
 void readList(std::vector<double>& v);
 void sortList(std::vector<double>& v);
-
+void mtRead(char* ptrb,double* ptra,int lengthLine,int arrSize, int nroThreads);
 int main(){
 
     std::vector<double> v;
@@ -28,25 +32,44 @@ int main(){
 }
 
 void readList(std::vector<double>& v){
-    std::string line;
-    std::ifstream myfile ("../data/list.txt");
-
-    std::cout<<"reading file\n";
-    auto start = std::chrono::high_resolution_clock::now();  
-    // unsync the I/O of C and C++.
-    // std::ios_base::sync_with_stdio(false);
-
-    if (myfile.is_open()){
-        while ( getline (myfile,line) ){
-            v.push_back(std::stod(line));
-        }
-        myfile.close();
-    }
-    else std::cout << "Unable to open file"; 
+    auto start = std::chrono::high_resolution_clock::now();
     
+    std::string pathFile = "../data/list.txt";
+    std::ifstream myFile (pathFile);
+    unsigned long int bufferSize = std::filesystem::file_size(pathFile);
+    std::cout << "File size is: "<< bufferSize << " bytes" << std::endl;
+    std::unique_ptr<char[]> buffer(new char[bufferSize]);
+    
+    if (myFile.is_open()){
+        myFile.read(buffer.get(), bufferSize);
+
+        //each line have one number with 22 digits + 1 \n = 23
+        int lengthLine = 23;
+        int arrSize = bufferSize/lengthLine;
+        std::unique_ptr<double[]> arr(new double[arrSize]);
+        
+        //multiThreading using 10 threads
+        int nroThreads = 10;
+        char* ptrB[nroThreads]; // pointer to buffer
+        double* ptrA[nroThreads]; // pointer to array
+        std::thread myThreads[nroThreads];
+        for(int i=0;i<nroThreads;i++){
+            ptrB[i]=&buffer[i*(bufferSize/nroThreads)];
+            ptrA[i]=&arr[i*(arrSize/nroThreads)];
+            myThreads[i] = std::thread(mtRead,ptrB[i],ptrA[i],lengthLine,arrSize,nroThreads);
+        }
+
+        for(int i=0;i<nroThreads;i++){
+            myThreads[i].join();
+        }
+
+        v.insert(v.begin(),ptrA[0],ptrA[0]+arrSize);
+    }
+    else {  std::cout << "Unable to open file"; }
+
     auto end = std::chrono::high_resolution_clock::now();
     double time_taken = ( std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() ) * 1e-9;
-    std::cout << time_taken << "sec\n";
+    std::cout << "reading file: "<< time_taken << "sec\n";
 }
 
 void sortList(std::vector<double>& v){
@@ -60,4 +83,16 @@ void sortList(std::vector<double>& v){
     auto end = std::chrono::high_resolution_clock::now();
     double time_taken = ( std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() ) * 1e-9;
     std::cout << time_taken << "sec\n";
+}
+
+void mtRead(char* ptrb, double* ptra, int lengthLine, int arrSize, int nroThreads){
+    for(int i=0;i<arrSize/nroThreads;i++){
+        char number[lengthLine-1];
+        std::memcpy(number,ptrb,lengthLine-1);
+        *ptra=std::atof(number);
+
+        ptrb += lengthLine;
+        ptra += 1;
+    }
+    // std::cout<< "id: "<<std::this_thread::get_id()<< " "<< *(ptrb+2) <<"\n";
 }
